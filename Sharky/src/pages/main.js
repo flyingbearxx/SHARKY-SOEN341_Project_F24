a703b34146b959c85ceb19122b133fc738922124
raghdaelkei <raghdaelkei72@gmail.com>
2024-09-28 23:53:43 -0400

    added team management page

--- Sharky/src/pages/main.js
+++ Sharky/src/pages/main.js
@@ -2,5 +2,16 @@ import SignUp from "./SignUp";
 import LogIn from "./LogIn";
 import Homepage from "./Homepage";
 import ContactUs from "./ContactUs";
+import ForgotPw from "./ForgotPw";
+import ResetPW from "./ResetPW";
+import TeamManagement from "./TeamManagement";
 
-export { LogIn, SignUp, Homepage, ContactUs };
+export {
+  LogIn,
+  SignUp,
+  Homepage,
+  ContactUs,
+  ForgotPw,
+  ResetPW,
+  TeamManagement,
+};
